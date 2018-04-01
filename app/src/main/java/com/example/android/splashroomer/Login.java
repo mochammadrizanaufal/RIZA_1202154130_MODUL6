eed7c03d351bed9f52f8c63e248901977eac85b9
Mochammad Riza Naufal <mochammadrizanaufal@gmail.com>
2018-04-01 23:30:24 +0700

    Push pertama

--- app/src/main/java/com/example/android/splashroomer/Login.java
+++ app/src/main/java/com/example/android/splashroomer/Login.java
@@ -2,10 +2,12 @@ package com.example.android.splashroomer;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,79 +16,136 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.example.android.splashroomer.model.User;
 
 public class Login extends AppCompatActivity {
 
-
-    private EditText inputEmail, inputPassword;
-    private FirebaseAuth auth;
+    private static final String TAG = Login.class.getSimpleName();
+    //declare variable
+    EditText mEmail, mPassword;
+    Button mSignIn, mSignUp;
+    String email, password;
+    //declare instance
+    FirebaseAuth mAuth;
+    DatabaseReference databaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //initialize instance
+        mAuth = FirebaseAuth.getInstance();
+        //bind data
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.txt_pass);
+        mSignIn = findViewById(R.id.btn_login);
+        mSignUp = findViewById(R.id.btn_signup);
 
-        auth = FirebaseAuth.getInstance();
-
-        if (auth.getCurrentUser() != null) {
-            startActivity(new Intent(Login.this, MainActivity.class));
-            finish();
-        }
-        setContentView(R.layout.activity_login);
-        inputEmail = (EditText) findViewById(R.id.email);
-        inputPassword = (EditText) findViewById(R.id.txt_pass);
-
-        //Get Firebase auth instance
-        auth = FirebaseAuth.getInstance();
-
+        databaseUser = FirebaseDatabase.getInstance().getReference(MainActivity.table_3);
+        //set on click button signin
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //mengambil isi field form
+                getTextField();
+                if (validateForm()){
+                    //panggil method login
+                    login(email, password);
+                }else {
+                    //toast pesan
+                    toastMessage("Please fill the form field");
+                }
+            }
+        });
+        //set on click button signup
+        mSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //mengambil isi field form
+                getTextField();
+                if (validateForm()){
+                    signUp(email, password);
+                }else {
+                    toastMessage("Please fill the form field");
+                }
+            }
+        });
     }
 
-
-
-    public void login(View view){
-
-        String email = inputEmail.getText().toString();
-        final String password = inputPassword.getText().toString();
-
-        if (TextUtils.isEmpty(email)) {
-            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
-            return;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //mengambil current user aktif
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //cek current user ada atau tidak
+        if (currentUser != null){
+            moveToMain();
         }
-
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+    }
+    //method signup
+    private void signUp(final String email, String pass){
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(
+                this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-
-                        if (!task.isSuccessful()) {
-                            // there was an error
-                            if (password.length() < 6) {
-                                inputPassword.setError(getString(R.string.minimum_password));
-                            } else {
-                                Toast.makeText(Login.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
-                            }
-                        } else {
-                            Intent intent = new Intent(Login.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
+                        //jika auth berhasil
+                        if (task.isSuccessful()){
+                            String id = mAuth.getCurrentUser().getUid();
+                            String[] username = email.split("@");
+                            User user = new User(id, username[0], email);
+                            databaseUser.child(id).setValue(user);
+                            moveToMain();
+                        }else {
+                            //toast pesan
+                            toastMessage("Something went wrong");
                         }
                     }
-                });
+                }
+        );
+    }
+    //method login
+    private void login(String email, String pass){
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(
+                this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            moveToMain();
+                        }else {
+                            //toast pesan
+                            toastMessage("Account not yet registered");
+                        }
+                    }
+                }
+        );
     }
 
+    private boolean validateForm(){
+        //cek apakah field email atau password kosong
+        if (email.isEmpty() || password.isEmpty()){
+            return false;
+        }else {
+            return true;
+        }
+    }
 
-
-        public void signup(View view){
-
-        Intent intent = new Intent(Login.this, Register.class);
+    //method get text
+    private void getTextField(){
+        email = mEmail.getText().toString();
+        password = mPassword.getText().toString();
+    }
+    //method intent to main activity
+    private void moveToMain(){
+        Intent intent = new Intent(Login.this, MainActivity.class);
         startActivity(intent);
+        finish();
+    }
+    //method toast pesan
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/android/splashroomer/MainActivity.java
+++ app/src/main/java/com/example/android/splashroomer/MainActivity.java
@@ -3,115 +3,103 @@ package com.example.android.splashroomer;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+import com.example.android.splashroomer.fragment.PagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth auth;
-    private Button signOut;
+    public static final String table_1 = "Post";
+    public static final String table_2 = "Comment";
+    public static final String table_3 = "User";
+    //declare instance
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initialize firebase instance
+        mAuth = FirebaseAuth.getInstance();
+        //bind data
+        TabLayout tab = findViewById(R.id.tab);
+        final ViewPager viewPager = findViewById(R.id.pager);
+        //set text for each tab
+        tab.addTab(tab.newTab().setText("Posts"));
+        tab.addTab(tab.newTab().setText("My Posts"));
+        //set tab to fill entire layout
+        tab.setTabGravity(tab.GRAVITY_FILL);
 
-        auth = FirebaseAuth.getInstance();
-
-        //get current user
-        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-
-        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tab.getTabCount());
+        //set viewpager adapter
+        viewPager.setAdapter(adapter);
+        //listener view pager saat berpindah
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tab));
+        //listener tab ketika dipilih
+        tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user == null) {
-                    // user auth state is changed - user is null
-                    // launch login activity
-                    startActivity(new Intent(MainActivity.this, Login.class));
-                    finish();
-                }
+            public void onTabSelected(TabLayout.Tab tab) {
+                //set view pager sesuai get posisi tab
+                viewPager.setCurrentItem(tab.getPosition());
             }
-        };
-        signOut = (Button) findViewById(R.id.btn_signout);
-        signOut.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View v) {
-                signOut();
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
-
     }
+    //ketika menu dibuat maka get menu inflate
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    //ketika opsi menu dipilih
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //get item id
+        int id = item.getItemId();
 
-
-
-    // this listener will be called when there is change in firebase user session
-    FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
-        @SuppressLint("SetTextI18n")
-        @Override
-        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-            FirebaseUser user = firebaseAuth.getCurrentUser();
-            if (user == null) {
-                // user auth state is changed - user is null
-                // launch login activity
-                startActivity(new Intent(MainActivity.this, Login.class));
-                finish();
-            }
+        if (id == R.id.logout){
+            mAuth.signOut();
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+            finish();
         }
-
-
-    };
-
-    //sign out method
-    public void signOut() {
-        auth.signOut();
-
-
-// this listener will be called when there is change in firebase user session
-        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user == null) {
-                    // user auth state is changed - user is null
-                    // launch login activity
-                    startActivity(new Intent(MainActivity.this, Login.class));
-                    finish();
-                }
-            }
-        };
+        return true;
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onStart() {
+    protected void onStart() {
         super.onStart();
-        auth.addAuthStateListener(authListener);
+        //mengambil current user aktif
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //cek current user ada atau tidak
+        if (currentUser == null){
+            Intent i = new Intent(MainActivity.this, Login.class);
+            startActivity(i);
+            finish();
+        }
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (authListener != null) {
-            auth.removeAuthStateListener(authListener);
-        }
+    //onClick add post
+    public void addPost(View view){
+        Intent i = new Intent(MainActivity.this, AddPostActivity.class);
+        startActivity(i);
     }
 }
+
